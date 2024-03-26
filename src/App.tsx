a9ae77592f16658dd30ea65afdeda1971eec3341
Evanilson Pereira <pevanilson99@gmail.com>
2024-03-26 16:03:47 -0300

    First commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+
+
+function App() {
+
+  return (
+    <main className="py-10 h-screen">
+      <h1 className="font-bold text-3x1 text-center">
+        Your todos
+      </h1>
+    </main>
+  )
+}
+
+export default App
