49b22a77fbf1f6a9629d59963dc704009536fbf3
Evanilson Pereira <pevanilson99@gmail.com>
2024-03-26 16:25:39 -0300

    Rendering lists with map method

---
+++ src/data/todos.ts
@@ -0,0 +1,23 @@
+import { Todo } from "../types/todo";
+
+export const dummyData: Todo[] = [
+
+    {
+        id: 1,
+        title: "Like this video",
+        completed: false
+    },
+
+    {
+        id: 2,
+        title: "Subscribe to coding in flow",
+        completed: false
+    },
+
+    {
+        id: 3,
+        title: "Leave a nice comment",
+        completed: false
+    },
+
+];
