9e505cd71f7683878773c26be2ae02a896b88af2
Evanilson Pereira <pevanilson99@gmail.com>
2024-03-27 20:10:57 -0300

    Cimponents, props and conditional  className

--- src/App.tsx
+++ src/App.tsx
@@ -5,9 +5,9 @@ import { dummyData } from "./data/todos"
 function App() {
 
   return (
-    <main className="py-10 h-screen">
+    <main className="py-10 h-screen space-y-5">
       <h1 className="font-bold text-3x1 text-center">Your todos</h1>
-      <div className="max-w-lg mx-auto">
+      <div className="max-w-lg mx-auto bg-slate-100 rounded-md p-9">
         <div className="space-y-1">
           {dummyData.map(todo => (
             <TodoItem todo={todo} />
