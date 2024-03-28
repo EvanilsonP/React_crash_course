d93e495b1cf1c6e7a895f481afea595c576eef5e
Evanilson Pereira <pevanilson99@gmail.com>
2024-03-28 20:30:10 -0300

    Add todo, form, controlled vs uncontrolled form

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import TodoItem from "./components/TodoItem"
 import { dummyData } from "./data/todos"
+import AddTodoForm from "./components/AddTodoForm";
 
 
 function App() {
@@ -12,10 +13,24 @@ function App() {
     )
   }
 
+  function addTodo(title: string) {
+    setTodos(prevTodos => [
+      {
+        id: prevTodos.length + 1,
+        title,
+        completed: false
+      },
+      ...prevTodos
+    ])
+  }
+
   return (
     <main className="py-10 h-screen space-y-5">
       <h1 className="font-bold text-3x1 text-center">Your todos</h1>
-      <div className="max-w-lg mx-auto bg-slate-100 rounded-md p-9">
+      <div className="max-w-lg mx-auto bg-slate-100 rounded-md p-5 space-y-5">
+        <AddTodoForm 
+        onSubmit={addTodo}
+        />
         <div className="space-y-1">
           {todos.map(todo => (
             <TodoItem 
